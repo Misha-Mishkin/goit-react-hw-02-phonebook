ad47451d029c426ee769bf489e9c74d9ccb0fde2
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-06-06 00:48:51 +0300

    update 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import ContactForm from "./ContactForm";
-import ContactList from "./ContactList";
-import Filter from "./Filter";
+// import ContactList from "./ContactList";
+// import Filter from "./Filter";
 
 
 export class App extends Component {
